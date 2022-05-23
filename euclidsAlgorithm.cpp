e1f20f438f42c076ff27e288521a57fd983871a2
Aro MIlitosyan <ar.militosyan.97@mail.ru>
2022-05-23 15:07:07 +0400

    Update euclidsAlgorithm.cpp

--- euclidsAlgorithm.cpp
+++ euclidsAlgorithm.cpp
@@ -1,4 +1,16 @@
 #include <iostream>
+int  largest_divider(int,int);
+
+int main(){
+    
+   int one_num{}; 
+   int two_num{};
+   std::cout << "Please input one_num integer numbers: "<<std::endl;
+   std::cin >> one_num;
+   std::cout << "Please input two_num integer numbers: "<<std::endl;
+   std::cin >> two_num;
+   std::cout << largest_divider(one_num,two_num);
+}
 
 int  largest_divider(int one_num,int  two_num){
   if (two_num == 0){
@@ -6,12 +18,3 @@ int  largest_divider(int one_num,int  two_num){
   }
     return largest_divider(two_num,(one_num%two_num) );
 }
-int main(){
-    
-   int one_num ; 
-   int two_num ;
-   std::cout << "Please input two integers numbers: "<<std::endl;
-   std::cin >> one_num >> two_num;
-   std::cout << largest_divider(one_num,two_num);
-}
-
